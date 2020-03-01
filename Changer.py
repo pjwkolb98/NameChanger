5a43267ea83a43ef84e038b0e9724e4595f9c600
Pieter <pjwkolb98@gmail.com>
2020-03-01 17:48:01 +0100

    v1.0

--- Changer.py
+++ Changer.py
@@ -1,7 +1,11 @@
 import os
 
-rootdir = 'C:/Users/Pieter/Documents/testmap'
+rootdir = 'C:/Users/Pieter/Documents/testmap/'
 
-for subdir in os.walk(rootdir):
-    print(rootdir.count())
-    print(subdir)
+for dir in os.listdir(rootdir):
+    print(dir)
+    new_name_split = dir.split(None, 1)
+    new_name = new_name_split.pop()
+    print(new_name)
+    os.rename(os.path.join(rootdir, dir),
+              os.path.join(rootdir, new_name))
